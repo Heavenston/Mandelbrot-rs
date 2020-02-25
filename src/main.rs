a77eda118a43ce4e5e3231f33702d7997f7ea610
Heavenston <malolegendrelemaire@gmail.com>
2020-02-25 16:24:47 +0100

    window with glutin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 extern crate winit;
-extern crate gl;
+extern crate glutin;
 
-use winit::{
+use glutin::{
   event::{Event, WindowEvent},
   event_loop::{ControlFlow, EventLoop},
   window::WindowBuilder,
+  ContextBuilder
 };
 
 fn main() {
   let event_loop = EventLoop::new();
-  let window = WindowBuilder::new()
-    .with_title("Heav's Mandelbrot")
-    .build(&event_loop).unwrap();
+  let window_builder = WindowBuilder::new()
+    .with_title("Heav's Mandelbrot");
+  
+  let windowed_context = ContextBuilder::new()
+    .build_windowed(window_builder, &event_loop)
+    .unwrap();
+  let windowed_context = unsafe { windowed_context.make_current().unwrap() };
 
   event_loop.run(move |event, _, control_flow| {
     *control_flow = ControlFlow::Poll;
@@ -20,9 +25,9 @@ fn main() {
       Event::WindowEvent {
         event: WindowEvent::CloseRequested,
         window_id,
-      } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+      } if window_id == windowed_context.window().id() => *control_flow = ControlFlow::Exit,
       Event::MainEventsCleared => {
-        window.request_redraw();
+        windowed_context.window().request_redraw();
       },
       Event::RedrawRequested(_) => {
         
