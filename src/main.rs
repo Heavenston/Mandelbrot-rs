382799eadc9b6da9c4e35a13ebbaf78fbba9f0ee
Heavenston <malolegendrelemaire@gmail.com>
2020-02-25 13:41:30 +0100

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate winit;
+extern crate gl;
+
 use winit::{
   event::{Event, WindowEvent},
   event_loop::{ControlFlow, EventLoop},
@@ -6,16 +9,24 @@ use winit::{
 
 fn main() {
   let event_loop = EventLoop::new();
-  let window = WindowBuilder::new().build(&event_loop).unwrap();
+  let window = WindowBuilder::new()
+    .with_title("Heav's Mandelbrot")
+    .build(&event_loop).unwrap();
 
   event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
+    *control_flow = ControlFlow::Poll;
 
     match event {
       Event::WindowEvent {
         event: WindowEvent::CloseRequested,
         window_id,
       } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+      Event::MainEventsCleared => {
+        window.request_redraw();
+      },
+      Event::RedrawRequested(_) => {
+        
+      },
       _ => (),
     }
   });
